38c1e79476496a874b24b137ff98fd25cc7f6613
Nyukers <nyukers@gmail.com>
2021-03-27 17:55:47 +0200

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,32 @@
+var data = JSON.parse(responseBody);
+
+var schema = {
+    "type": "object",
+    //specify requires properties
+    "required": ["name","homeworld","films","species","vehicles","starships"],
+    //describe the properties
+    "properties":{
+        "name":      {"type":"string"},
+        "homeworld": {"type":"string"},
+        "films":     {
+            "type":"array",
+            "items":{"type":"string"}
+            },
+        "species":   {
+            "type":"array",
+            "items":{"type":"string"}
+            },
+        "vehicles":  {
+            "type":"array",
+            "items":{"type":"string"}
+            },
+        "starships": {
+            "type":"array",
+            "items":{"type":"string"}
+            },
+    }
+};
+
+pm.test('Body is correct.', function() {
+    pm.expect(tv4.validate(data, schema)).to.be.true;
+});
